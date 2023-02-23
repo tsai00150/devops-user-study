40028c43f6627357ceeefa3b2bff407e3e8a8b5a
tsai00150 <tsai00150@outlook.com>
2023-02-23 15:29:39 -0500

    full answer

--- code/two_sum.py
+++ code/two_sum.py
@@ -1,10 +1,42 @@
 def twoSum(nums: list, target: int):
-    #should return correct
+    # 0
+    # d = {}
+    # for i in range(len(nums)):
+    #     d[nums[i]] = i
+
+    # for i in range(len(nums)):
+    #     if target-nums[i] in d:
+    #         return [i, d[target-nums[i]]]
+    
+    # 1
+    # d = {}
+    # for i in range(len(nums)):
+    #     d[nums[i]] = i
+
+    # for i in range(len(nums)):
+    #     if target-nums[i] in d:
+    #         return [i, d[target-nums[i]]]
+    # return [-1]
+
+    # 2
+    # d = {}
+    # for i in range(len(nums)):
+    #     if target-nums[i] in d:
+    #         return [d[target-nums[i]], i]
+    #     d[nums[i]] = i
+    # return [-1]
+
+    # 3
+    for i in range(len(nums)):
+        if type(nums[i]) != int:
+            try:
+                nums[i] = int(nums[i])
+            except:
+                return [-1]
+
     d = {}
     for i in range(len(nums)):
-        d[nums[i]] = i
-
-    for i in range(len(nums)):
         if target-nums[i] in d:
-            return [i, d[target-nums[i]]]
-    
+            return [d[target-nums[i]], i]
+        d[nums[i]] = i
+    return [-1]
--- test_two_sum.py
+++ test_two_sum.py
@@ -8,23 +8,37 @@ class testTwoSum(unittest.TestCase):
     # https://docs.python.org/3/library/unittest.html
 
     def test_givenExample(self):
-        msg = "Passed the given example. "
+        msg = '''Passed the given example. '''
         self.assertEqual(twoSum([2,7,11,15], 9), [0,1], msg)
     
-    # def test_noAnswer(self):
-    #     msg = '''Need to return [-1] if no answer found. 
-    #     Input: [100, 100, 100], 9
-    #     Expected output: [-1]
+    # This is just a template; you can change the format whatever you want to have better communication with the developer. 
+    # def test_<write the test name here>(self):
+    #     msg = '''<message when there is an error>
+    #     <try to communicate with the developer in this section>
     #     '''
-    #     self.assertEqual(twoSum([100, 100, 100], 9), [-1], msg)
+    #     self.assertEqual(twoSum(<the nums array>, <target>), [-1], msg)
+    
+    # 1
+    def test_noAnswer(self):
+        msg = '''Need to return [-1] if no answer found. 
+        Input: [100, 100, 100], 9
+        Expected output: [-1]
+        '''
+        self.assertEqual(twoSum([100, 100, 100], 9), [-1], msg)
 
-
-    # def test_duplicateIndex(self):
-    #     msg = '''Might have duplicate index. Input: [3,4], 6 
-    #     Expected output: [0, 1]
-    #     '''
-    #     self.assertEqual(twoSum([3,4], 6), [0,1], msg)
-
+    #2
+    def test_duplicateIndex(self):
+        msg = '''Might have duplicate index. Input: [3,4], 6 
+        Expected output: [-1]
+        '''
+        self.assertEqual(twoSum([3,4], 6), [-1], msg)
+    
+    #3
+    def test_wrongType(self):
+        msg = '''If nums has a string that can be converted to integer type, 
+        convert it and treat it as an integer; otherwise, return [-1]. 
+        '''
+        self.assertEqual(twoSum([2,'7',11,15], 9), [0, 1], msg)
 
 if __name__ == '__main__':
     unittest.main()
