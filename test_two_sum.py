b1fcae5753e9722d29f8bbb4983cc77874a15e00
tsai00150 <tsai00150@outlook.com>
2023-02-23 00:09:28 -0500

    init commit

---
+++ test_two_sum.py
@@ -0,0 +1,30 @@
+import unittest
+from two_sum import twoSum
+
+class testTwoSum(unittest.TestCase):
+
+    # the function name need to start with test_
+    # more documentation can be seen here:
+    # https://docs.python.org/3/library/unittest.html
+
+    def test_givenExample(self):
+        msg = "Passed the given example. "
+        self.assertEqual(twoSum([2,7,11,15], 9), [0,1], msg)
+    
+    def test_noAnswer(self):
+        msg = '''Need to return [-1] if no answer found. 
+        Input: [100, 100, 100], 9
+        Expected output: [-1]
+        '''
+        self.assertEqual(twoSum([100, 100, 100], 9), [-1], msg)
+
+
+    def test_duplicateIndex(self):
+        msg = '''Might have duplicate index. Input: [3,4], 6 
+        Expected output: [0, 1]
+        '''
+        self.assertEqual(twoSum([3,4], 6), [0,1], msg)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ two_sum.py
@@ -0,0 +1,9 @@
+def twoSum(nums: list, target: int):
+    d = {}
+    for i in range(len(nums)):
+        d[nums[i]] = i
+
+    for i in range(len(nums)):
+        if target-nums[i] in d:
+            return [i, d[target-nums[i]]]
+    
