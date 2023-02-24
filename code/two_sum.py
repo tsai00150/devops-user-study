1265dc756dce1443a9ac53a715b34acd46f633b7
tsai00150 <tsai00150@outlook.com>
2023-02-23 20:39:53 -0500

    initial state

--- code/two_sum.py
+++ code/two_sum.py
@@ -1,10 +1,2 @@
 def twoSum(nums: list, target: int):
-    #should return correct
-    d = {}
-    for i in range(len(nums)):
-        d[nums[i]] = i
-
-    for i in range(len(nums)):
-        if target-nums[i] in d:
-            return [i, d[target-nums[i]]]
-    
+    pass
--- test_two_sum.py
+++ test_two_sum.py
@@ -6,24 +6,18 @@ class testTwoSum(unittest.TestCase):
     # the function name need to start with test_
     # more documentation can be seen here:
     # https://docs.python.org/3/library/unittest.html
+    # https://www.dataquest.io/blog/unit-tests-python/
 
     def test_givenExample(self):
         msg = "Passed the given example. "
         self.assertEqual(twoSum([2,7,11,15], 9), [0,1], msg)
-    
-    # def test_noAnswer(self):
-    #     msg = '''Need to return [-1] if no answer found. 
-    #     Input: [100, 100, 100], 9
-    #     Expected output: [-1]
-    #     '''
-    #     self.assertEqual(twoSum([100, 100, 100], 9), [-1], msg)
 
-
-    # def test_duplicateIndex(self):
-    #     msg = '''Might have duplicate index. Input: [3,4], 6 
-    #     Expected output: [0, 1]
+    # This is just a template; you can change the format whatever you want to have better communication with the developer. 
+    # def test_<write the test name here>(self):
+    #     msg = '''<message when there is an error>
+    #     <try to communicate with the developer in this section>
     #     '''
-    #     self.assertEqual(twoSum([3,4], 6), [0,1], msg)
+    #     self.assertEqual(twoSum(<the nums array>, <target>), [-1], msg)
 
 
 if __name__ == '__main__':
