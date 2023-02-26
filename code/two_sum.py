f32d59f18ba7c5170983138feeae3ee053cd8dbb
You-Wen Luo <youwenl1@uci.edu>
2023-02-26 10:32:19 -0800

    fix no answer bug

--- code/two_sum.py
+++ code/two_sum.py
@@ -7,5 +7,7 @@ def twoSum(nums: list, target: int):
         if target - num in seen:
             print([seen[num], seen[target - num]])
             return [seen[num], seen[target - num]]
+    print(-1)
+    return -1
 
-twoSum([2,7,11,15], 9)
+twoSum([2,8,11,15], 9)
