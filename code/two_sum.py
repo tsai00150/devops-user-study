50f766583282e728792c6780537010b13a8729a3
You-Wen Luo <youwenl1@uci.edu>
2023-02-26 10:34:34 -0800

    fix null array bug

--- code/two_sum.py
+++ code/two_sum.py
@@ -1,13 +1,15 @@
 def twoSum(nums: list, target: int):
+    if not nums:
+        return None
     seen = {}
     for i, num in enumerate(nums):
         seen[num] = i
 
     for num in seen:
         if target - num in seen:
-            print([seen[num], seen[target - num]])
+            #print([seen[num], seen[target - num]])
             return [seen[num], seen[target - num]]
-    print(-1)
+    #print(-1)
     return -1
 
 twoSum([2,8,11,15], 9)
