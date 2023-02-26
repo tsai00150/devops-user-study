a28eb8580f5bb9499dd771a98cdbf65aa7e7c53a
LukCabbage <102343867+LukCabbage@users.noreply.github.com>
2023-02-26 01:15:44 +0000

    <3

--- code/two_sum.py
+++ code/two_sum.py
@@ -1,2 +1,8 @@
 def twoSum(nums: list, target: int):
-    pass
+    d = {}
+    for i in range(len(nums)):
+        d[nums[i]] = i
+
+    for i in range(len(nums)):
+        if target-nums[i] in d:
+            return [i, d[target-nums[i]]]
