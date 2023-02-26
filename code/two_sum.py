bd711e60082a7f2b30a7ccc2321ae5c1a17f9004
Ryan <cdpss9401@gmail.com>
2023-02-26 22:18:37 +0800

    first commit

--- code/two_sum.py
+++ code/two_sum.py
@@ -1,2 +1,12 @@
 def twoSum(nums: list, target: int):
-    pass
+    length = len(nums)
+    dictionary = {}
+    for i in range(0,length):
+        dictionary[nums[i]]=i
+    for j in range(0,length):
+        res = target - nums[j]
+        if res in dictionary: 
+            print([j,dictionary[res]])
+            return [j,dictionary[res]]
+        
+twoSum([2,7,11,15],9)
