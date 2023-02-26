1064d93201a532b227aedf08d501e9314b081922
You-Wen Luo <youwenl1@uci.edu>
2023-02-26 10:28:32 -0800

    finish two_sum

--- code/two_sum.py
+++ code/two_sum.py
@@ -1,2 +1,11 @@
 def twoSum(nums: list, target: int):
-    pass
+    seen = {}
+    for i, num in enumerate(nums):
+        seen[num] = i
+
+    for num in seen:
+        if target - num in seen:
+            print([seen[num], seen[target - num]])
+            return [seen[num], seen[target - num]]
+
+twoSum([2,7,11,15], 9)
