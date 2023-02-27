9948ebcb4c626dd6a161ed6d73204d405a1495d3
pauleezz <yspan0702@gmail.com>
2023-02-28 01:38:03 +0800

    third

--- code/two_sum.py
+++ code/two_sum.py
@@ -1,14 +1,7 @@
 def twoSum(nums: list, target: int):
     if (not nums or len(nums) == 1):
         return
-    d = dict()
-    for i in range(len(nums)):
-        d[nums[i]] = i
-    
-    for each in d:
-        diff = target - each
-        if diff in d.keys():
-            return [d[each], d[diff]]
-
-nums = []
-target = 9
+    for idx, num in enumerate(nums):
+        diff = target - num
+        if diff in nums[idx+1:]:
+            return [idx, idx + nums[idx+1:].index(diff) + 1]
