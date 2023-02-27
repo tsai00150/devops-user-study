79776d785fc7acffc3138a87250322bf53b81a5e
pauleezz <yspan0702@gmail.com>
2023-02-28 01:23:53 +0800

    first step

--- code/two_sum.py
+++ code/two_sum.py
@@ -1,2 +1,12 @@
 def twoSum(nums: list, target: int):
-    pass
+    d = dict()
+    for i in range(len(nums)):
+        d[nums[i]] = i
+    
+    for each in d:
+        diff = target - each
+        if diff in d.keys():
+            return [d[each], d[diff]]
+
+nums = [2,7,11,15]
+target = 9
