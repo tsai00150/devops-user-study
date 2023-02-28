7b66ef910c460b8409a88614bc9b6972f7ace480
tsai00150 <tsai00150@outlook.com>
2023-02-28 13:25:55 -0500

    step 0

--- code/two_sum.py
+++ code/two_sum.py
@@ -1,2 +1,16 @@
 def twoSum(nums: list, target: int):
-    pass
+    dictionary = {}
+    
+    for i in range(0,len(nums)):
+        if nums[i] in dictionary:
+            dictionary[nums[i]].append(i)
+        else:
+            dictionary[nums[i]] = [i]
+
+    for i in range(0,len(nums)):
+        curr_num = nums[i]
+        diff = target - curr_num
+        if diff in dictionary:
+            return [i, dictionary[diff][0]]
+    
+    return None
